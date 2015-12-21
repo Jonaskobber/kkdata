c0a24d78782d07a352228bfba36f020ddc3991cb
Jonas Kobberø <jonaskobber92@gmail.com>
2015-12-21 10:20:32 +0100

    React-router added

--- src/app.js
+++ src/app.js
@@ -1,16 +1,40 @@
 import './styles/main.scss'
 
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
 import { render } from 'react-dom'
+import { Router, Route, Link } from 'react-router'
 
 import * as containers from './containers'
 
 const {
-  /*Borgere,*/
+  Borgere,
   Skatteliste
 } = containers
 
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <div>
+          <h1>App</h1>
+          <Link to='/skatteliste'>Skat</Link>
+          <Link to='/borgere'>Borgere</Link>
+        </div>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+App.propTypes = {
+  children: PropTypes.object
+}
+
 render(
-  <Skatteliste />,
+  <Router>
+    <Route path='/' component={App}>
+      <Route path='/skatteliste' component={Skatteliste} />
+      <Route path='/Borgere' component={Borgere} />
+    </Route>
+  </Router>,
   document.getElementById('app')
 )
